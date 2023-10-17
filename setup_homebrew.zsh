521e8dee8cdd1dc84dd7f41595c0a5bfc79e7467
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2023-10-16 22:01:09 -0700

    Switched to brewfile for brew installs
    Installed visual studio code
    Installed firefox developer edition
    
    Modified setup_homebrew.zsh to run brew bundle in verbose mode

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,10 +4,5 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install httpie
-brew install bat
-brew install --no-quarantine visual-studio-code
-brew install --no-quarantine alfred
-brew install --no-quarantine firefox
-brew tap homebrew/cask-versions
-brew install --cask --no-quarantine firefox-developer-edition
+brew bundle --verbose
+
