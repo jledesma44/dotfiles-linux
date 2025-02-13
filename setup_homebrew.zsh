d545cbdb4415e7dc016bea1102ffb163967f5265
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-02-12 22:08:11 -0800

    removed the conditional statement the setup_homebrew script and added apps to install

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,15 +1,19 @@
 #!/usr/bin/env zsh
 
-if exist brew; then
-  echo "brew exists, skipping install"
-else
-  echo "brew doesn't exist, continuing with install"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
 echo "\n<<< Starting Homebrew Setup >>>\n"
+# if exist brew; then
+#   echo "brew exists, skipping install"
+# else
+#   echo "brew doesn't exist, continuing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# fi
+brew install httpie
+brew install bat
+
+brew install --cask visual-studio-code
 
 
 
-brew bundle --verbose
+
+# brew bundle --verbose
 
