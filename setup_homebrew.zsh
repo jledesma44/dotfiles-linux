dd939c5a12a0e7cdd783223745cc941f2bc68bb5
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-02-14 21:08:17 -0800

    added a comment to remind me of the --no-quarantine solution for homebrew

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,5 +1,12 @@
 #!/usr/bin/env zsh
 
+#TODO - Keep an eye out for a different --no-quarantine solution.
+#Currently you can't do the 'brew bundle --no-quarantine' option.
+#It's currently exported in zshrc:
+#export HOMEBREW_CASK_OPTS=--no-quarantine
+#https://github/Homebrew/homebrew-bundle/issues/474
+
+
 echo "\n<<< Starting Homebrew Setup >>>\n"
 # if exist brew; then
 #   echo "brew exists, skipping install"
