2e3bccb9b95756e9022c8d757f9708ee8bed355d
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-03-30 14:30:06 -0700

    uncommented tailwindcss.lua file

--- Lazyvim/lua/plugins/tailwindcss.lua
+++ Lazyvim/lua/plugins/tailwindcss.lua
@@ -1,34 +1,34 @@
--- return {
---   {
---     "neovim/nvim-lspconfig",
---     opts = {
---       servers = {
---         tailwindcss = {},
---       },
---     },
---   },
---   {
---     "NvChad/nvim-colorizer.lua",
---     config = function()
---       require("colorizer").setup({
---         user_default_options = {
---           tailwind = true,
---         },
---       })
---     end,
---   },
---   {
---     "hrsh7th/nvim-cmp",
---     dependencies = {
---       { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
---     },
---     opts = function(_, opts)
---       -- Original Lazyvim kink icon formatter
---       local format_kinds = opts.formatting.format
---       opts.formatting.format = function(entry, item)
---         format_kinds(entry, item)
---         return require("tailwinds-colorizer-cmp").formatter(entry, item)
---       end
---     end,
---   },
--- }
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {},
+      },
+    },
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({
+        user_default_options = {
+          tailwind = true,
+        },
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+    },
+    opts = function(_, opts)
+      -- Original Lazyvim kink icon formatter
+      local format_kinds = opts.formatting.format
+      opts.formatting.format = function(entry, item)
+        format_kinds(entry, item)
+        return require("tailwinds-colorizer-cmp").formatter(entry, item)
+      end
+    end,
+  },
+}
