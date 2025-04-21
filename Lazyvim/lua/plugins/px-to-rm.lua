f22c623c8d1ea59be38792b2b0df27f763b75539
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-04-21 16:57:06 -0700

    added the px-to-rm plugin in replacement of the unito plugin

---
+++ Lazyvim/lua/plugins/px-to-rm.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "jsongerber/nvim-px-to-rem",
+    -- If you need to set some options replace the line above with:
+    config = function()
+      require("nvim-px-to-rem").setup()
+    end,
+  },
+}
