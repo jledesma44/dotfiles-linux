06b177bf791e576734a1057834df53ceb2f29625
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-07-20 16:50:56 -0700

    Modified the setup_homebrew.zsh script to work with both intel and arm base macs

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/bin/zsh
 
 #TODO - Keep an eye out for a different --no-quarantine solution.
 #Currently you can't do the 'brew bundle --no-quarantine' option.
@@ -7,14 +7,30 @@
 #https://github/Homebrew/homebrew-bundle/issues/474
 
 
-echo "\n<<< Starting Homebrew Setup >>>\n"
-if exists brew; then
-  echo "brew exists, skipping install"
-else
-  echo "brew doesn't exist, continuing with install"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo "\n<<< Starting Homebrew Setup >>>\n"
+# if exists brew; then
+#   echo "brew exists, skipping install"
+# else
+#   echo "brew doesn't exist, continuing with install"
+#   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# fi
+
+
+# Add Homebrew to PATH regardless of architecture
+if [[ -d "/opt/homebrew/bin" ]]; then
+    export PATH="/opt/homebrew/bin:$PATH"
+elif [[ -d "/usr/local/bin" ]]; then
+    export PATH="/usr/local/bin:$PATH"
 fi
 
+# Now check for Homebrew
+if command -v brew >/dev/null 2>&1; then
+    echo "***** Homebrew is already installed *******"
+    echo "***** Now checking for any updates!!! ******"
+else
+    echo "Installing Homebrew..."
+    # Your installation code
+fi
 
 brew bundle --verbose
 
