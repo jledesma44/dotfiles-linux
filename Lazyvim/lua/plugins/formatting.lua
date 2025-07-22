a6c86ce504b0a383bd7199d7793e0e30a7425912
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-07-22 13:22:37 -0700

    Added plugins for formatting and linting react
    1. formatting.lua
    2.linting.lua
    3.lsp.lua
    4.treesitter.lua

---
+++ Lazyvim/lua/plugins/formatting.lua
@@ -0,0 +1,18 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettierd", "prettier" },
+      javascriptreact = { "prettierd", "prettier" },
+      typescript = { "prettierd", "prettier" },
+      typescriptreact = { "prettierd", "prettier" },
+      json = { "prettierd", "prettier" },
+      css = { "prettierd", "prettier" },
+      html = { "prettierd", "prettier" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+  },
+}
---
+++ Lazyvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "javascript",
+      "typescript",
+      "tsx",
+      "jsx",
+      "json",
+      "css",
+      "html",
+    },
+  },
+}
