38036e7d7a94aeaac4c2e1a99f93606bc3595091
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-07-26 01:27:24 -0700

    added the keybind "d"  for deleting without yanking keybinding

--- Lazyvim/lua/config/keymaps.lua
+++ Lazyvim/lua/config/keymaps.lua
@@ -1,4 +1,4 @@
--- Keymaps are automatically loaded on the VeryLazy event
+-- Keymaps are automatically loaded onthe VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps e
 
@@ -33,4 +33,4 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to clipboard",
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to clipboard", noremap = true, silent = true })
 
 -- delete without yanking
-vim.keymap.set({ "n", "v" }, "<leader>", [["_d]], { desc = "Delete without yanking", noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yanking", noremap = true, silent = true })
