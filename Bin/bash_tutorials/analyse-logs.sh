e2a42d15044b0d356112f5e03b709eaaab2c1e5d
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-08-22 13:29:05 -0700

    added a practice bash script

--- Bin/bash_tutorials/analyse-logs.sh
+++ Bin/bash_tutorials/analyse-logs.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-find . -name "log.*" -mtime -1
+LOG_DIR="/Users/jaimeledesma/logs"
+APP_LOG_FILE="application.log"
+SYS_LOG_FILE="system.log"
+
+echo "analysing log files"
+echo "======================="
+
+ehco -e "\nList of log files updated in the last 24 hours"
+
+find LOG_DIR -name "log.*" -mtime -1
+
+echo
 
 grep "ERROR" application.log
 grep -c "ERROR" application.log
