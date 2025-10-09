dd585999da81996c494d443a9a7262a66694eeb7
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-10-09 12:11:02 -0700

    added styling to the package install script

---
+++ packages_install_update.zsh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+#
+
+# Colors for output
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+set -e  # Exit on error
+
+echo "######################################################################################"
+echo "               ${YELLOW}!!  Package Installation / Updates !!${NC}                         "
+echo "######################################################################################"
+
+#Updates and upgrades the Arch linux local repositories
+sudo pacman -Syu
+
+#Install/Updates packages from a list.
+sudo pacman -S - < pkglist.txt
+
+echo "${GREEN}All packages have been installed and or updated${NC}"
