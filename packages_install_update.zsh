954f7e00e6aa300e1f7640cdbc4b5f1dbec13d60
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-10-26 21:52:43 -0700

    modified packges_install_update.zsh
    removed the scripts to enbale services for tailscale and syncthing from
    this file to separate individual scripts.

--- packages_install_update.zsh
+++ packages_install_update.zsh
@@ -22,4 +22,19 @@ sudo pacman -S - < pkglist_native.txt
 #kmonad - change file permissions to run as user when using exec-once command
 sudo chmod u+s /usr/bin/kmonad
 
+echo "######################################################################################"
+echo "               ${YELLOW}!!  Enabling Syncthing Services !!${NC}                         "
+echo "######################################################################################"
+
+#Start syncthing services
+sudo systemctl enable --now syncthing@jledesma44.service 
+
+echo "######################################################################################"
+echo "               ${YELLOW}!!  Enabling Tailscale Services !!${NC}                         "
+echo "######################################################################################"
+#Start Tailscaled
+sudo systemctl enable --now tailscaled
+
+#Set ssh in tailscaled
+sudo tailscale set --ssh
 echo "${GREEN}All packages have been installed and or updated${NC}"
