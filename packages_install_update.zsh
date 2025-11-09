5685c2cb870b340b21c0ddc497d85e113c50a8d4
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-11-09 11:44:28 -0800

    modified packages_install-update script
    1.added --noconfirm flag to script
    2.added script to upate pkglist_native.txt after running the
    packages_install_update script if new packages where installed
    3.removed scripts to start services for tailscale and syncthing. Created
    separate scripts for these service start ups.

--- packages_install_update.zsh
+++ packages_install_update.zsh
@@ -17,24 +17,12 @@ echo "##########################################################################
 sudo pacman -Syu
 
 #Install/Updates packages from a list.
-sudo pacman -S - < pkglist_native.txt
+sudo pacman -S --noconfirm - < pkglist_native.txt
+
+#Add any newly installed packages to pkglist_native.txt file
+pacman -Qqe > pkglist_native.txt
 
 #kmonad - change file permissions to run as user when using exec-once command
 sudo chmod u+s /usr/bin/kmonad
 
-echo "######################################################################################"
-echo "               ${YELLOW}!!  Enabling Syncthing Services !!${NC}                         "
-echo "######################################################################################"
-
-#Start syncthing services
-sudo systemctl enable --now syncthing@jledesma44.service 
-
-echo "######################################################################################"
-echo "               ${YELLOW}!!  Enabling Tailscale Services !!${NC}                         "
-echo "######################################################################################"
-#Start Tailscaled
-sudo systemctl enable --now tailscaled
-
-#Set ssh in tailscaled
-sudo tailscale set --ssh
 echo "${GREEN}All packages have been installed and or updated${NC}"
