6e40772b817144e1776b0e288b6234a428fef8b2
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-11-09 11:49:39 -0800

    separated tailscale-services froom packages_install-update script to its own script

--- tailscale-services.zsh
+++ tailscale-services.zsh
@@ -1,12 +1,10 @@
 #Enabling Tailscale servcies
 echo "######################################################################################"
-echo "               ${YELLOW}!!  Enabling Tailscale Services !!${NC}                         "
+echo "               ${YELLOW}!!  Enabling Tailscale Services !!${NC}                      "
 echo "######################################################################################"
 #Start Tailscaled
 sudo systemctl enable --now tailscaled
 
 #Set login
-sudo tailscale up
+sudo tailscale up --ssh
 
-#Set ssh in tailscale
-sudo tailscale set --ssh
